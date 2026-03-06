c6663acea1275589d5b55056872e1a986f5374a6
unknown <dev@DSN1191348076.edu119.local>
2026-03-06 10:47:59 -0300

    layout

--- lib/screens/widgets_layout.dart
+++ lib/screens/widgets_layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:new_project/screens/widgets_conteudo.dart';
+// import 'package:new_project/screens/widgets_conteudo.dart';
+import 'package:new_project/widgets/titulo_sessao.dart';
 
 class WidgetsLayout extends StatelessWidget {
   const WidgetsLayout({super.key});
@@ -7,17 +8,66 @@ class WidgetsLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Widgets Layout'),
-      ),
+      appBar: AppBar(title: const Text('Widgets Layout')),
       body: ListView(
-       padding: EdgeInsets.all(16.0),
-       children: [
-          ElevatedButton(onPressed: () => Navigator.push(context,
-          MaterialPageRoute(builder: (_) => WidgetsConteudo())), 
-          child: Text('Widgets de Conteúdo')),
-
-       ],
+        padding: EdgeInsets.all(16.0),
+        children: [
+          TituloSessao(titulo: 'padding'),
+          Container(
+            color: const Color.fromARGB(255, 152, 255, 138),
+            child: Padding(
+              padding: EdgeInsetsGeometry.all(20),
+              child: Text('Texto com padding'),
+            ),
+          ),
+          Divider(),
+          TituloSessao(titulo: 'Align'),
+          Container(
+            height: 80,
+            color: const Color.fromARGB(188, 100, 151, 199),
+            alignment: Alignment.center,
+            child: Text('Texto centralizado'),
+          ),
+          Divider(),
+          TituloSessao(titulo: 'Center'),
+          Container(
+            height: 80, 
+            color: const Color.fromARGB(255, 117, 179, 165),
+            child: Center(child: Text('Texto centralizado')),
+            ),
+            Divider(),
+            TituloSessao(titulo: 'SizedBox'),
+            Column(
+              children: [
+                Text('Texto acima do SizedBox'),
+                SizedBox(height: 20),
+                Text('Texto abaixo do SizedBox'),
+              ],
+            ),
+            Divider(),
+            TituloSessao(titulo: 'Expanded e Flexible'),
+            Container(
+              height: 200,
+              color: const Color.fromARGB(255, 255, 213, 0),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Container(
+                      color: const Color.fromARGB(255, 255, 149, 149),
+                      child: Center(child: Text('Expanded')),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      color: const Color.fromARGB(255, 219, 11, 11),
+                      child: Text('Flexible (1)'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+        ],
       ),
     );
   }
