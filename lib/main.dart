adcfa5b3e192d693a25fd7b15cb955a3e596c757
unknown <dev@DSN1191348076.edu119.local>
2026-03-13 11:06:27 -0300

    melhorando o codigo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:new_project/screens/widgets_conteudo.dart';
 import 'package:new_project/screens/widgets_layout.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -20,29 +19,71 @@ class MyApp extends StatelessWidget {
 }
 
 class ListContents extends StatelessWidget {
-  const ListContents({super.key});
+  final List<CatalogoItem> secoes = [
+    CatalogoItem(
+    titulo: 'Widgets de Conteudo',
+    icone: Icons.text_fields,
+    descricao: 'Exemplos de widgets basicos',
+    destino: WidgetsConteudo(),
+    ),
+    CatalogoItem(
+    titulo: 'Widgets de layout',
+    icone: Icons.text_fields,
+    descricao: 'Exemplos/demonstraçoes',
+    destino: WidgetsLayout(),
+    ),
+  ];
+
+  ListContents({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Bem-Vindo', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold))),
+      appBar: AppBar(title: Text('Bem-Vindo', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold))
+      ),
       body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () => Navigator.push(
-              context,
-           MaterialPageRoute(builder: (_) => WidgetsConteudo())), 
-          child: Text('Widgets de Conteúdo')
+        children: secoes.map(
+          (item) => GestureDetector(
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (_) => item.destino),
+              );
+            },
+        child: Card(
+          child:  Padding(padding: EdgeInsets.all(12),
+          child: Column(
+            children: [
+              Icon(item.icone, size:48, color: Colors.blue,),
+              Text(item.titulo, textAlign: TextAlign.center,),
+              Text(item.descricao, textAlign: TextAlign.center, style: 
+              TextStyle(fontSize: 12, color: Colors.amber),),
+            ],
           ),
-           ElevatedButton(
-            onPressed: () => Navigator.push(
-             context,
-             MaterialPageRoute(builder: (_) => WidgetsLayout())),
-           child: Text('Widgets de Layout')),
-
-
-        ],
+          ),
+        ),
+        )
+       ).toList(),
       ),
     );
   }
 }
+
+class CatalogoItem{
+  final String titulo;
+  final IconData icone;
+  final String descricao;
+  final Widget destino;
+
+  CatalogoItem({
+    required this.titulo,
+    required this.icone,
+    required this.descricao,
+    required this.destino,
+  });
+}
+
+
+//  ElevatedButton(
+//             onPressed: () => Navigator.push(
+//               context,
+//            MaterialPageRoute(builder: (_) => WidgetsConteudo())
+//            ), 
